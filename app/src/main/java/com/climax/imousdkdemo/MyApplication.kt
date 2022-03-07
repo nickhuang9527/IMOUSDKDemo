3e3b44f5ecf694448c37c7b4d4c18d63a8dbed2a
Nick Huang <nickhuang@climax.com.tw>
2022-03-07 18:03:42 +0800

    [feature] Load libraries

--- app/src/main/java/com/climax/imousdkdemo/MyApplication.kt
+++ app/src/main/java/com/climax/imousdkdemo/MyApplication.kt
@@ -14,5 +14,22 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        try {
+//            System.loadLibrary("c++_shared")
+            System.loadLibrary("netsdk")
+            System.loadLibrary("configsdk")
+            System.loadLibrary("jninetsdk")
+            System.loadLibrary("gnustl_shared")
+            System.loadLibrary("LechangeSDK")
+            System.loadLibrary("SmartConfig")
+            System.loadLibrary("SoftAPConfig")
+            System.loadLibrary("GMCrypto")
+            System.loadLibrary("LCOpenApiClient")
+        } catch (var1: Exception) {
+            System.err.println("loadLibrary Exception$var1")
+        } catch (var2: Error) {
+            System.err.println("loadLibrary Exception$var2")
+        }
     }
 }
