9262fa249cb41178f30b648f538b3a4cf7af71cd
Nick Huang <nickhuang@climax.com.tw>
2022-03-07 15:34:19 +0800

    [feature] Init lechange openSDK

--- app/src/main/java/com/climax/imousdkdemo/MainActivity.kt
+++ app/src/main/java/com/climax/imousdkdemo/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import com.lechange.opensdk.api.InitParams
+import com.lechange.opensdk.api.LCOpenSDK_Api
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +14,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mSearchDeviceInitInfoButton: Button
     private lateinit var mInitDeviceByIpButton: Button
     private lateinit var mGetSoftApWifiListButton: Button
+
+    private var openapiUrl = "openapi-sg.easy4ip.com:443"
+    private val userToken = "Ut_0000236cc6c3fe8644ea953cf8d730c6"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +34,8 @@ class MainActivity : AppCompatActivity() {
 
         mInitSDKButton.setOnClickListener {
             Log.d(TAG, "Init lechange openSDK")
+            val initParams = InitParams(this.applicationContext, openapiUrl, userToken)
+            LCOpenSDK_Api.initOpenApi(initParams)
         }
 
         mConnectToApOfCamButton.setOnClickListener {
